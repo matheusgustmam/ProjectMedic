c5b33e5e26532d428feea3142a85fc003fd1b21e
Matheus <Matheus@PC>
2025-02-25 21:58:40 -0300

    Inplementados metodos para cadastro e edição de medicos e pacientes

--- src/main/java/com/mycompany/mavenproject1/viwer/TelaDoAtestado.java
+++ src/main/java/com/mycompany/mavenproject1/viwer/TelaDoAtestado.java
@@ -192,10 +192,10 @@ public class TelaDoAtestado extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        try{
+      /*  try{
             JasperReport AtestadoCompilado
                     = JasperCompileManager.compilert("");
-        }
+        }   */
     }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
